fa18fde991319bffba0584f0094950d96c05d4e4
simonachess <simonachess@gmail.com>
2022-05-12 15:35:04 +0300

    fixes

--- src/js/app.js
+++ src/js/app.js
@@ -5,7 +5,7 @@ window.addEventListener("DOMContentLoaded", () => {
 	document.querySelector("img").setAttribute("src", "https://picsum.photos/200/300")
 	document.querySelector(".image").addEventListener("click", clicked) 
 	function clicked(){
-		document.querySelector("img").style = "transform: scale(2)"
+		document.querySelector(".image").style = "transform: scale(2)"
 	}
 
   const button = document.querySelector(".button");
