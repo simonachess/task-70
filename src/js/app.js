e6dd995c232601578deb478f62a2481e237b55fd
simonachess <simonachess@gmail.com>
2022-05-12 15:12:36 +0300

    fixed

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,12 @@
 import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
-  // This block will be executed once the page is loaded and ready
+
+	document.querySelector("img").setAttribute("src", "https://picsum.photos/200/300")
+	document.querySelector("img").addEventListener("click", clicked) 
+	function clicked(){
+		document.querySelector("img").style = "transform: scale(2)"
+	}
 
   const button = document.querySelector(".button");
   button.addEventListener("click", () => {
